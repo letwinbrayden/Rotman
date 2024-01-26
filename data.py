db45396d7c2c73ea2716b2e1d024be63f80400a0
Brayden <bletwin@ualberta.ca>
2024-01-25 22:11:12 -0700

    Added Data, Portfolio.py

---
+++ data.py
@@ -0,0 +1,10 @@
+class Data:
+
+    def __init__(self, database):
+        self.database = database
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ portfolio.py
@@ -0,0 +1,10 @@
+class Portfolio:
+
+    def __init__(self, portfolio):
+        self.portfolio = portfolio
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
